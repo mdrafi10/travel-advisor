c1070f1ab9dab4c5eca3157a30719b0932f4c36f
mdrafi10 <mdrafi101010101010@gmail.com>
2022-02-23 17:13:31 +0600

    travel-advisor-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { CssBaseline, Grid } from "@material-ui/core";
+import { getPlacesData, getWeatherData } from "./api";
+import Header from "./Components/Header/Header";
+import List from "./Components/List/List";
+import Map from "./Components/Map/Map";
 
 function App() {
+  const [places, setPlaces] = useState([]);
+  const [childClicked, setChildClicked] = useState(null);
+  const [coordinates, setCoordinates] = useState({});
+  const [bounds, setBounds] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [type, setType] = useState("restaurants");
+  const [rating, setRating] = useState("");
+  const [filteredPlaces, setFilteredPlaces] = useState([]);
+  const [weatherData, setWeatherData] = useState([]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => {
+        setCoordinates({ lat: latitude, lng: longitude });
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    const filteredPlaces = places?.filter(
+      (place) => Number(place.rating) > rating
+    );
+    setFilteredPlaces(filteredPlaces);
+  }, [rating]);
+
+  useEffect(() => {
+    if (bounds?.sw && bounds?.ne) {
+      setIsLoading(true);
+      // console.log(coordinates, bounds);
+      getWeatherData(coordinates?.lat, coordinates?.lng).then((data) =>
+        setWeatherData(data)
+      );
+
+      getPlacesData(type, bounds?.sw, bounds?.ne).then((data) => {
+        setPlaces(
+          data?.filter((place) => place?.name && place?.num_reviews > 0)
+        );
+        setFilteredPlaces([]);
+        setIsLoading(false);
+      });
+    }
+  }, [type, bounds]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <CssBaseline />
+      <Header setCoordinates={setCoordinates} />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          <List
+            isLoading={isLoading}
+            childClicked={childClicked}
+            places={filteredPlaces?.length ? filteredPlaces : places}
+            type={type}
+            setType={setType}
+            rating={rating}
+            setRating={setRating}
+          />
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          md={8}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <Map
+            setChildClicked={setChildClicked}
+            setBounds={setBounds}
+            setCoordinates={setCoordinates}
+            coordinates={coordinates}
+            places={filteredPlaces?.length ? filteredPlaces : places}
+            weatherData={weatherData}
+          />
+        </Grid>
+      </Grid>
     </div>
   );
 }
---
+++ src/api/index.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+
+export const getPlacesData = async (type, sw, ne) => {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(
+      `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
+      {
+        params: {
+          bl_latitude: sw?.lat,
+          bl_longitude: sw?.lng,
+          tr_longitude: ne?.lng,
+          tr_latitude: ne?.lat,
+        },
+        headers: {
+          "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
+          "x-rapidapi-key": process.env.REACT_APP_RAPID_API_KEY,
+        },
+      }
+    );
+    // 0fc3e455e5msh927fccc6a0539bcp179ecdjsn002fd4488362... old
+    // b19769b2e0mshbd29dd4a16a4dd8p1f6f25jsn630b043999f5... new
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getWeatherData = async (lat, lng) => {
+  try {
+    if (lat && lng) {
+      const { data } = await axios.get(
+        "https://community-open-weather-map.p.rapidapi.com/find",
+        {
+          params: { lat, lon: lng },
+          headers: {
+            "x-rapidapi-key": process.env.REACT_APP_RAPID_API_KEY,
+            "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+          },
+        }
+      );
+
+      return data;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
